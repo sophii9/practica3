28961befb2436086a28294b63f069870cd0158d6
Sofía Monsiváis <angelasantillan445@gmail.com>
2025-10-27 22:05:44 -0500

    Comienzo práctica 3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
